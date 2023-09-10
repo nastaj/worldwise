cf0c0fe222ffa90605448c90afb2221282df02a4
nastaj <G00424689@atu.ie>
2023-09-10 12:52:14 +0200

    routes and cities

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "../components/Logo";
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -13,6 +16,11 @@ function PageNav() {
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Log in
+          </NavLink>
         </li>
       </ul>
     </nav>
